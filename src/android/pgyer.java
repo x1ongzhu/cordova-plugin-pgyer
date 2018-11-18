38972cbab44e6b69645cd115f3d4c52a7d858f29
x1ongzhu <692949348@qq.com>
2018-11-18 21:38:36 +0800

    cordova-android@7.0

--- src/android/pgyer.java
+++ src/android/pgyer.java
@@ -1,5 +1,8 @@
 package cn.x1ongzhu.pgyer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.util.Log;
 
 import com.pgyersdk.update.PgyUpdateManager;
@@ -8,6 +11,7 @@ import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 
 import org.apache.cordova.CordovaWebView;
+import org.json.JSONException;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -16,6 +20,36 @@ public class pgyer extends CordovaPlugin {
     @Override
     public void initialize(final CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(0);
+        } else {
+            checkUpdate();
+        }
+    }
+
+    @Override
+    public void requestPermissions(int requestCode) {
+        super.requestPermissions(requestCode);
+        cordova.requestPermissions(this, requestCode,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE});
+    }
+
+    @Override
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+        super.onRequestPermissionResult(requestCode, permissions, grantResults);
+        boolean granted = true;
+        for (int res : grantResults) {
+            if (res != PackageManager.PERMISSION_GRANTED) {
+                granted = false;
+                break;
+            }
+        }
+        if (granted) {
+            checkUpdate();
+        }
+    }
+
+    private void checkUpdate() {
         cordova.getThreadPool().execute(new Runnable() {
             public void run() {
                 boolean force = preferences.getBoolean("forceUpdate", false);
